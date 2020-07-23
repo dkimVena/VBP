2219b3252426308cb96fe3277a253eeebde8defa
goginim <david.ingook@gmail.com>
2020-07-22 20:35:11 -0400

    fix small

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/images/logo.png',
-  pathPrefix: `/gatsby-starter-grayscale/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/vbp-sanitizing/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'VBP Sanitizing',
   subHeading: 'Virus Block and Protect',
 
